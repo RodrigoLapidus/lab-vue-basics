48b25edd98127e7a770f2a031350c9899d0012e9
Rodrigo Lopez <rodrigo.lapidus@gmail.com>
2022-08-09 18:14:03 +0200

    upload of the first labs in Vue

---
+++ app/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav>
+      <h1 v-bind:style=" {
+        padding: '50px',
+        backgroundColor: 'lightgray',
+        color: 'white',
+      }">This is the navbar</h1>
+    </nav>
+    
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <MainComponent />
+    
+    <footer v-bind:style=" {
+      padding: '30px',
+      backgroundColor: 'lightgrey',
+      color: 'white',
+    }">
+      <h1>This is the footer</h1>
+    </footer>
+
+  </div>
+
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import MainComponent from "./components/MainComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    MainComponent,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ app/src/components/MainComponent.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <h3>{{ 2 + 2 }}</h3>
+        <br>
+        <h3>{{ stringToPrint }}</h3>
+        <br>
+        <h3 v-show="thisIsTrue">
+        This is being shown because the condition is true
+        </h3>
+
+        <h4 v-for="blogPost in blogPosts" :key="blogPost">The title of the book is {{ blogPost.title }}
+        <br>
+        The description of the book is {{ blogPost.description }}
+        <br>
+        The content of the book is {{ blogPost.content }}
+        </h4>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            stringToPrint: "Aloha",
+
+            thisIsTrue: true,
+
+            blogPosts: [
+                { title: "I Robot", description: "Book about robots", content: "Placeholder text 1"},
+                { title: "How to kill a Mockingbird", description: "Book about rights", content: "Placeholder text 2"},
+                {title: "The Man in the high Castle", description: "Book about authoritarian governments", content: "Placeholder text 3"}
+            ],
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
